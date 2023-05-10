79091930d4d5cb82a01a73c0287f9dd7def4cdaa
Jigar Patel <jigar@automationzen.com>
2023-05-09 21:27:36 -0500

    add youtubeasset id field

--- models/normalized_baskets.sql
+++ models/normalized_baskets.sql
@@ -6,14 +6,14 @@
     )
 }}
 
-select
-    id,
-    {{dollars_to_cents('totalprice')}} as total_amount_in_cents,
-    created::timestamp,
-               _airbyte_emitted_at,
+select id,
+       {{dollars_to_cents('totalprice')}} as total_amount_in_cents,
+       created::timestamp,
+    paymenttransactionid as payment_transaction_id,
+
+       _airbyte_emitted_at,
        _airbyte_ab_id,
        _airbyte_baskets_hashid
-
 
 
 from {{ source("raw_synchtank", "baskets") }}
