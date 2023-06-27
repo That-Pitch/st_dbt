9c63ebdb38312dad0c06eb2efe051ee699ddc058
Jigar Patel <jigar@automationzen.com>
2023-06-27 15:05:14 -0500

    update nab table to set nac as fk

--- models/normalized_baskets_comp.sql
+++ models/normalized_baskets_comp.sql
@@ -3,7 +3,8 @@
         post_hook=[set_primary_key("nbc_id_pk", "basket_id,item_id"),
         set_foreign_key("nb_id_fk","basket_id","{{ ref('normalized_baskets') }} (id)"),
         set_foreign_key("nbi_id_fk","item_id","{{ ref('normalized_basket_items') }} (id)"),
-        set_foreign_key("nt_id_fk","track_id","{{ref('normalized_tracks')}} (id)")
+        set_foreign_key("nt_id_fk","track_id","{{ref('normalized_tracks')}} (id)"),
+        set_foreign_key("nac_id_fk","artist_id, user_id","{{ref('normalized_artists_comp')}} (artist_id, user_id)")
         ]
     )
 }}
@@ -22,8 +23,8 @@ b.created::timestamp,
     {{ dollars_to_cents("bi.priceataddition") }} as price_at_addition,
     bi.track as track_id,
     t.is_exclusive as exclusive_track,
-    a.artist_name,
-    a.sub_name as artist_subscription_name,
+    ac.artist_id,
+    ac.user_id,
     bi.gatewayrefundid as gateway_refund_id,
     bi._airbyte_emitted_at,
     bi._airbyte_ab_id,
@@ -31,7 +32,8 @@ b.created::timestamp,
 from {{ref('normalized_baskets')}} b
 inner join {{ref('normalized_basket_items')}} bi  using (_airbyte_baskets_hashid)
 left join {{ref('normalized_tracks')}} t on t.id = bi.track
-left join {{ref('normalized_artists_comp')}} a on a.artist_id = t.artist
+left join {{ref('normalized_artists_comp')}} ac on ac.artist_id = t.artist
+
 
 
 
