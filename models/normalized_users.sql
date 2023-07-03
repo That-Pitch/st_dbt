d71ead8b473f829e286d6f59a479b4f306990f52
Jigar Patel <jigar@automationzen.com>
2023-07-03 12:37:00 -0500

    add membership col to users

--- models/normalized_users.sql
+++ models/normalized_users.sql
@@ -1,28 +1,24 @@
-{{
-    config(
-        post_hook=[set_primary_key("nu_id_pk","id")
-
-        ]
-    )
-}}
+{{ config(post_hook=[set_primary_key("nu_id_pk", "id")]) }}
 
 
-
-
-select id,
-       name,
-       created::timestamp,
-       artists, --1 user can be associated with multiple artists but all share the same subscription.
-       email,
-        role,
-       lastmodified::timestamp as last_modified,
-       (activesubscriptions[0]->>'id')::bigint as subscription_id,
-       (activesubscriptions[0]->>'name')::varchar as subscription_name,
-       (activesubscriptions[0]->'artistCollectionTitles') as subscription_collections,
-       _airbyte_emitted_at,
-       _airbyte_ab_id,
-       _airbyte_users_hashid
+select
+    id,
+    name,
+    created::timestamp,
+    artists,  -- 1 user can be associated with multiple artists but all share the same subscription.
+    email,
+    role,
+    lastmodified::timestamp as last_modified,
+    (activesubscriptions[0] ->> 'id')::bigint as subscription_id,
+    (activesubscriptions[0] ->> 'name')::varchar as subscription_name,
+    (activesubscriptions[0] -> 'artistCollectionTitles') as subscription_collections,
+    case
+        when (activesubscriptions[0] ->> 'name')::varchar like '%Pro%'
+        then true
+        else false
+    end as is_pro_membership,
+    _airbyte_emitted_at,
+    _airbyte_ab_id,
+    _airbyte_users_hashid
 from {{ source("raw_synchtank", "users") }}
 order by created desc
-
-
