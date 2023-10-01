9e8d4dabca49f50b9df494fdd63bd95069f11e04
Jigar Patel <jigar@automationzen.com>
2023-09-30 22:30:01 -0500

    add jt id

--- models/job_payments.sql
+++ models/job_payments.sql
@@ -2,12 +2,24 @@
     config(
         post_hook=[
             set_primary_key("job_payments_pk", "id"),
-            set_foreign_key("nu_id_fk", "user_id", "{{ ref('normalized_users') }} (id)"),
-            set_foreign_key("na_id_fk", "artist_id", "{{ ref('normalized_artists') }} (id)"),
-            set_foreign_key("nt_id_fk", "track_id", "{{ ref('normalized_tracks') }} (id)"),
-            set_foreign_key("np_id_fk", "purchase_id", "{{ ref('normalized_purchases') }} (id)"),
-                        set_foreign_key("nl_id_fk", "license_id", "{{ ref('normalized_licenses') }} (id)"),
-
+            set_foreign_key(
+                "nu_id_fk", "user_id", "{{ ref('normalized_users') }} (id)"
+            ),
+            set_foreign_key(
+                "na_id_fk", "artist_id", "{{ ref('normalized_artists') }} (id)"
+            ),
+            set_foreign_key(
+                "nt_id_fk", "track_id", "{{ ref('normalized_tracks') }} (id)"
+            ),
+            set_foreign_key(
+                "np_id_fk", "purchase_id", "{{ ref('normalized_purchases') }} (id)"
+            ),
+            set_foreign_key(
+                "nl_id_fk", "license_id", "{{ ref('normalized_licenses') }} (id)"
+            ),
+            set_foreign_key(
+                "jt_id_fk", "job_id", "{{ source('jobs','job_tracker') }} (id)"
+            ),
         ]
     )
 }}
