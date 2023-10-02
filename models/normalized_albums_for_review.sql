aff5db5220b8642e396a09cc55bc1fd62a140f1b
Jigar Patel <jigar@automationzen.com>
2023-10-02 16:00:12 -0500

    update types for review

--- models/normalized_albums_for_review.sql
+++ models/normalized_albums_for_review.sql
@@ -1,9 +1,45 @@
 {{
     config(
-        post_hook=[set_primary_key("nalfr_id_pk","id"),
-
-    ]
+        post_hook=[
+            set_primary_key("nalfr_id_pk", "id"),
+        ]
     )
 }}
 
-select * from {{source('raw_synchtank','reviewalbums')}}
+
+select
+createdbyname,
+currentreviewstatus,
+featured,
+metadata,
+submittedforreview,
+reviewrels,
+submittedforreviewby,
+jpgimage,
+title,
+originallypublished,
+artistnames,
+submittedforreviewbyname,
+publishedenddate,
+metadatatitles,
+previousreviewstatus,
+reviewed,
+id::bigint,
+categories,
+_accessgranted,
+titlewithfeaturedartistsandversion,
+publishedstartdate,
+created,
+format,
+drafts,
+reviewedby,
+customalbumid,
+createdby,
+reviewedbyname,
+hash,
+_airbyte_ab_id,
+_airbyte_emitted_at,
+_airbyte_normalized_at,
+_airbyte_reviewalbums_hashid
+
+from {{ source("raw_synchtank", "reviewalbums") }}
--- models/normalized_artists_for_review.sql
+++ models/normalized_artists_for_review.sql
@@ -1,9 +1,35 @@
 {{
     config(
-        post_hook=[set_primary_key("nafr_id_pk","id"),
-
-    ]
+        post_hook=[
+            set_primary_key("nafr_id_pk", "id"),
+        ]
     )
 }}
 
-select * from {{source('raw_synchtank','reviewartists')}}
+select
+_accessgranted,
+_airbyte_ab_id,
+_airbyte_emitted_at,
+_airbyte_normalized_at,
+_airbyte_reviewartists_hashid,
+created,
+createdby,
+createdbyname,
+currentreviewstatus,
+drafts,
+hash,
+id::bigint,
+metadata,
+metadatatitles,
+previousreviewstatus,
+reviewed,
+reviewedby,
+reviewedbyname,
+reviewrels,
+submittedforreview,
+submittedforreviewby,
+submittedforreviewbyname
+
+
+from
+    {{ source("raw_synchtank", "reviewartists") }}
